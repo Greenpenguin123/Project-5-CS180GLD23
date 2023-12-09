4ca99d8bb440052b275ffd3fb121cd581aba7a5d
noellemrocks <143757995+noellemrocks@users.noreply.github.com>
2023-12-09 17:57:58 -0500

    Create Testing.java

---
+++ Testing.java
@@ -0,0 +1,83 @@
+public class Testing {
+    public static void main(String[] args) {
+    //IN ORDER OF README
+
+    //Consumer Methods
+
+        //Consumer.java
+
+            //viewMarketPlace
+                Consumer.viewMarketPlace();
+                /*
+                Prints out a list of products, in specified format
+
+
+
+                 */
+
+            //getProductList
+
+            //searchProducts
+
+            //addProduct
+
+            //removeProduct
+
+            //printPurchaseHistory
+
+            //purchaseProduct
+
+            //clearCart
+
+            //exportPurchaseHistory
+
+            //viewDashboard
+
+            //sortDashboard
+
+            //showDescription
+
+    //Seller Methods
+
+        //StoreplaceManager.java
+
+            //displayDashboard
+
+            //createProduct
+
+            //editProduct
+
+            //deleteProduct
+
+            //exportProduct
+
+            //printItemsPurchasedStatistics
+
+            //calculateStoreSales
+
+            //calculateProductSales
+
+        //SellerDashboard.java
+
+            //displayDashboard
+
+    //Market UI
+
+        //UserAccountManager.java
+
+            //incrementItemsSoldBy
+
+            //toString
+
+            //printForSort
+
+            //gatherCreate
+
+            //gatherSignIn
+
+            //logout
+
+            //deleteUser
+
+    }
+}
